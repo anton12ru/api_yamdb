dfa16a233f61412f51cd2ac67e4b3d3363cc285c
Di0niz <moskovchenkody@gmail.com>
2022-03-15 18:40:16 +0300

    #2 применен блек (#3)
    Co-authored-by: Denis Moskovchenko <di0niz@yandex-team.ru>

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,35 +1,29 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
 
 
 class Review(models.Model):
     text = models.TextField()
-    title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='reviews')
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name="reviews")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
     score = models.IntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=('title',),
-                name='unique_title'
-            )
-        ]
+        constraints = [models.UniqueConstraint(fields=("title",), name="unique_title")]
 
     def __str__(self):
         return self.text
 
 
 class Comments(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments')
+        Review, on_delete=models.CASCADE, related_name="comments"
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
